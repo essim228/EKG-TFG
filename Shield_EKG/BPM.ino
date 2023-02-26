847880e7a01f7d60b483127fa1a3d3eb65b86f24
essim228 <106173055+essim228@users.noreply.github.com>
2023-02-26 14:32:25 +0100

    Update BPM.ino

--- Shield_EKG/BPM.ino
+++ Shield_EKG/BPM.ino
@@ -1,6 +1,5 @@
 #include <SoftwareSerial.h>
 //Parameters
-SoftwareSerial espSerial(5, 6);
 #define ecgPin A0 // Analog input pin for EKG signal
 int ecgValue = 0; // Variable to store the EKG reading
 unsigned long ecgTime = 0; // Time of the last EKG peak
@@ -12,28 +11,27 @@ int bpm = 0; // Beats per minute
 void setup() {
   //Init Serial USB
   Serial.begin(9600);
-  espSerial.begin(9600);
+  BTserial.begin(9600);
   Serial.println(F("Initialize System"));
   //Init AnalogSmooth
-  pinMode(aisPin, INPUT);
+  pinMode(ecgPin, INPUT);
 }
 
 void loop() {
   
   
-  Serial.println(bpm());
-  espSerial.print(bpm());
+  Serial.println(beats());
 }
 
 
-int bpm(){
+int beats() {
   ecgValue = analogRead(ecgPin); // Read the EKG signal
   if (ecgValue > 650) { // If the signal is above a certain threshold
     if ((millis() - ecgTime) > 10) { // If enough time has passed since the last peak
       ecgInterval = millis() - ecgTime; // Calculate the time between peaks
       ecgTime = millis(); // Update the time of the last peak
       bpm = 60000 / ecgInterval; // Calculate beats per minute
-      return bpm // Return the BPM value
+      return bpm; // Return the BPM value
       
     }
   }
